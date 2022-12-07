0b00797080d25b399ef844803915c9d9bc0bb986
Dontcode44 <kabutto378@gmail.com>
2022-12-06 19:02:13 -0500

    [ADD] - Fields to save UserId and citiName

--- src/transactions/entities/transactions-register.entity.ts
+++ src/transactions/entities/transactions-register.entity.ts
@@ -1,14 +1,20 @@
 import { City } from "src/cities/entities/cities.entity";
 import { User } from "src/users/entities/users.entity";
-import { Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Transaction {
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Column()
+  savedUserId: string;
+
   @OneToMany(() => User, (user) => user.transactions)
   user: User;
+
+  @Column()
+  savedCityId: string;
 
   @OneToMany(() => City, (city) => city.transactions)
   city: City;

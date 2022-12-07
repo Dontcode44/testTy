9f8a589d2c13a5efe7e4bf3ff33c6f993bae456e
Dontcode44 <kabutto378@gmail.com>
2022-12-06 19:28:19 -0500

    [ADD] - Comments

--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,6 +1,7 @@
 import { City } from 'src/cities/entities/cities.entity';
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 
+/* The Restaurant class has a name and a city. The city is a foreign key to the City class */
 @Entity()
 export class Restaurant {
   @PrimaryGeneratedColumn()

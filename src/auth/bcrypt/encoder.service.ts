ccdd0f95f1c02b67746cdde169bff2d0cc11145d
Dontcode44 <kabutto378@gmail.com>
2022-12-06 19:32:49 -0500

    [ADD] - Comments to the encoder service 👾

--- src/auth/bcrypt/encoder.service.ts
+++ src/auth/bcrypt/encoder.service.ts
@@ -3,11 +3,23 @@ import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class BcryptEncoderService {
+  /**
+   * It generates a random salt, then hashes the password using that salt
+   * @param {string} password - The password to hash.
+   * @returns A promise that resolves to a string.
+   */
   async hashPassword(password: string): Promise<string> {
     const salt = await bcrypt.genSalt(10);
     return bcrypt.hash(password, salt);
   }
 
+  /**
+   * It takes a password and a hashed password, and returns a promise that resolves to true if the
+   * password matches the hashed password, and false otherwise
+   * @param {string} password - The password that the user entered.
+   * @param {string} hashedPassword - The hashed password that was stored in the database.
+   * @returns A promise that resolves to a boolean.
+   */
   async comparePassword(
     password: string,
     hashedPassword: string,

673f8b63030358cccd1c585c038f90b570d6b55b
Dontcode44 <kabutto378@gmail.com>
2022-12-06 19:04:33 -0500

    [ADD] - Endpoint to show transactions released

--- src/transactions/transactions.controller.ts
+++ src/transactions/transactions.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { TransactionsService } from './transactions.service';
 
 @Controller('transactions')
-export class TransactionsController {}
+export class TransactionsController {
+  constructor(
+    private readonly transactionsService: TransactionsService,
+  ) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Get()
+  async getAllTransactions() {
+    return await this.transactionsService.getAllTransactions();
+  }
+}
--- src/transactions/transactions.service.ts
+++ src/transactions/transactions.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Transaction } from './entities/transactions-register.entity';
 
 @Injectable()
-export class TransactionsService {}
+export class TransactionsService {
+  constructor(
+    @InjectRepository(Transaction)
+    private readonly transactionsRepository: Repository<Transaction>,
+  ) {}
+
+  async getAllTransactions(): Promise<Transaction[]> {
+    return await this.transactionsRepository.find();
+  }
+}
